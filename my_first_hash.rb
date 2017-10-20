8f2faa5a95a5ea96be160c1a109b8ad87c96c617
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-20 15:35:15 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,9 +7,9 @@ end
 
 def shipping_manifest
   shipping_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
-    "oil paintings" => 3
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3,
     }
 end
 
